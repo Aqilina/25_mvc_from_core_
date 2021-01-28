5fbd420161d0ec3501713cec3048190cb85854f4
akvilina <akvilina.lapenaite@gmail.com>
2021-01-28 08:29:33 +0200

    config file with our details. user sql

---
+++ app/config/config.php
@@ -0,0 +1,23 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', '_YOUR_DB_USER_');
+define('DB_PASS', '_YOUR_DB_PASS_');
+define('DB_NAME', '25_mvc_from_core');
+
+// aprrot wil be used when we need absloute path to our app dir
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL ROOT will be the path in the url
+define('URLROOT', 'http://localhost/php/25_mvc_from_core');
+
+// Site name 
+define('SITENAME', 'From Core');
+
+
+// need to change .htaccess in public
+// RewriteBase /__YOUR_SITE_DIR__/public
+// replace  __YOUR_SITE_DIR__ with root dir name of your site 
+
+
+//25_mvc_from_core
