3a3396764f61a170f96e3f8b3456c170a76b2805
akvilina <akvilina.lapenaite@gmail.com>
2021-02-01 08:21:21 +0200

    fixes color

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -1,17 +1,20 @@
 <?php
 //session helper for displaying useful feedback to the user
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!
+session_start();
+
 //flash msg helper
-
 //example save msg flash('register_success', 'Your registrations is successful');
-//for display flash register('register_success'); - nuskaitymui
+//for display flash('register_success'); - nuskaitymui
 
-function flash($name = '', $message = '', $class = 'alert alert-success') {
+function flash($name = '', $message = '', $class = 'alert alert-success')
+{
     if (!empty($name)) {
-        //this is the part where we set the message
+        // this is the part where we set the message
         if (!empty($message) && empty($_SESSION[$name])) {
 
-            //if there is some left in session we unset it
+            // if there is some class left in session we unset it
             if (!empty($_SESSION[$name . '_class'])) {
                 unset($_SESSION[$name . '_class']);
             }
@@ -19,10 +22,11 @@ function flash($name = '', $message = '', $class = 'alert alert-success') {
             $_SESSION[$name] = $message;
             $_SESSION[$name . '_class'] = $class;
 
-            //this is where we display msg to the wiew
+            // this is where we display msg to the view
         } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : ''; // be jo zinute nebus raudona
             echo "<div class='$class' id='msg-flash'>{$_SESSION[$name]}</div>";
-            //unset values that have been shown
+            // unset the values that have been shown
             unset($_SESSION[$name]);
             unset($_SESSION[$name . '_class']);
         }
