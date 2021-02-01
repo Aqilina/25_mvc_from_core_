46eb42c3bf825975533c48f66af7f1fbe7d48d7d
akvilina <akvilina.lapenaite@gmail.com>
2021-02-01 11:40:41 +0200

    created add method and loading view

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -26,4 +26,16 @@ class Posts extends Controller
        ];
        $this->view('posts/index', $data);
     }
+
+    public function add()
+    {
+        $data = [
+            'title' => '',
+            'body' => '',
+            'titleErr' => '',
+            'bodyErr' => ''
+        ];
+
+        $this->view('posts/add', $data);
+    }
 }
