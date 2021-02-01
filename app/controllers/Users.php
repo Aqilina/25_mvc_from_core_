0eec31f6abf6127e78aa1de0d424c4398a69259f
akvilina <akvilina.lapenaite@gmail.com>
2021-02-01 08:44:07 +0200

    logout method created

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -215,4 +215,14 @@ class Users extends Controller
 
         redirect('/pages/index');
     }
+
+    public function logout() {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+
+        redirect('/users/login');
+
+    }
 }
