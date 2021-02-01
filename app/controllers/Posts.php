8801853c4950c0496c77f7a6047dba4c802621be
akvilina <akvilina.lapenaite@gmail.com>
2021-02-01 11:00:48 +0200

    created post model, used getPosts and send data to the view

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -8,9 +8,22 @@
 
 class Posts extends Controller
 {
+    private $postModel;
+
+    public function __construct() {
+        //restrict access of this controller only to logged in users
+        if (!isLoggedIn()) redirect('/users/login');
+
+        $this->postModel = $this->model('Post');
+    }
+
     public function index()
     {
-       $data = [];
+        //get posts
+        $posts = $this->postModel->getPosts();
+       $data = [
+           'posts' => $posts
+       ];
        $this->view('posts/index', $data);
     }
 }
