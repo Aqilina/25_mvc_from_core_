406e4ca497a0c83dba804d40de8a9b0f98f8dc18
akvilina <akvilina.lapenaite@gmail.com>
2021-02-02 10:58:23 +0200

    edit method with validation and correct data flow

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -100,4 +100,72 @@ class Posts extends Controller
         ];
         $this->view('posts/show', $data);
     }
+
+
+    public function edit($id=null)
+    {
+        //if post has no such parameter, we redirect
+        if ($id=== null) redirect('/posts');
+
+        //if form was submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//            VALIDATION. ISVALOMAS POST MASYVAS
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'titleErr' => '',
+                'bodyErr' => ''
+            ];
+
+            //VALIDATE TITLE
+            if (empty($data['title'])) {
+                $data['titleErr'] = 'Please enter a title';
+            }
+
+            //VALIDATE BODY
+            if (empty($data['body'])) {
+                $data['bodyErr'] = 'Please enter a text';
+            }
+
+            //CHECK IF THERE ARE NO ERRORS
+            if (empty($data['titleErr']) && empty($data['bodyErr'])) {
+                //there are no errors
+                if ($this->postModel->updatePost($data)) {
+                    //post added
+                    flash('post_message', 'You have edited a new post');
+                    redirect('/posts');
+                } else {
+                    die('something went wrong in adding post');
+                }
+            } else {
+//                load view with errors
+                $this->view('posts/edit', $data);
+            }
+
+        } else {
+            //user entered this page
+            $post = $this->postModel->getPostById($id);
+
+            if ($post) {
+                //check if this post belongs to this user
+                if ($post->user_id !== $_SESSION['user_id']) redirect('/posts');
+                //post found and will load the view
+            } else
+                die('sth went wrong in getpostById()');
+
+
+            $data = [
+                'id' => $id,
+                'title' => $post->title,
+                'body' => $post->body,
+                'titleErr' => '',
+                'bodyErr' => ''
+            ];
+
+            $this->view('posts/edit', $data);
+        }
+    }
 }
