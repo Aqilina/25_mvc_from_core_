20e5b5539b137155cc0a9e6bd55fd0274285973c
akvilina <akvilina.lapenaite@gmail.com>
2021-02-02 09:25:48 +0200

    show view with current datya

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -2,9 +2,9 @@
 require APPROOT . '/views/inc/header.php';
 ?>
 
-<h1>This is single page template for the post <?php echo $data['post_id']?></h1>
+<h1>This is single page template for the post <?php echo $data['post']->id?></h1>
 
-
+<?php var_dump($data['post']);?>
 <?php
 require APPROOT . '/views/inc/footer.php';
 ?>
