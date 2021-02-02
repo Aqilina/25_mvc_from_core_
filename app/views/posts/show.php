6ace429e8ebbe10590d08ac6b0b6c9b02b676091
akvilina <akvilina.lapenaite@gmail.com>
2021-02-02 10:42:20 +0200

    show edit and delete only if post belongs to current user

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -16,7 +16,26 @@ require APPROOT . '/views/inc/header.php';
 <p class="lead"><?php echo $data['post']->body?></p>
 
 
+<!-------------------------------------------------------------------------------------------------------->
+<!--SHOW THIS ONLY IF THIS POST BELONGS TO THIS!!! USER-->
+<?php //var_dump($data['post']->user_id);?>
+<?php //var_dump($data['post']);?>
+<?php //var_dump($data['user']);?>
+<?php //var_dump($_SESSION);?>
+<hr>
 
+
+<?php if ($data['post']->user_id === $_SESSION['user_id']) : ?>
+<!--PARODO PARAM ID-->
+<a href="<?php echo URLROOT . '/posts/edit/' . $data['post']->id?>" class="btn btn-info"><i class="fa fa-pencil"></i> Edit</a>
+
+<!--forma apdorjama posts/delete-->
+<form action="<?php echo URLROOT?>/posts/delete" method="post" class="pull-right">
+    <button type="submit" class="btn btn-danger"><i class="fa fa-close"></i> Delete</button>
+</form>
+<?php
+endif;
+?>
 
 <?php
 require APPROOT . '/views/inc/footer.php';
