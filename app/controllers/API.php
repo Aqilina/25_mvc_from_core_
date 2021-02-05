6df0ff1affb8d998bd1ef45cf62752a5941087b1
akvilina <akvilina.lapenaite@gmail.com>
2021-02-05 10:25:32 +0200

    get commnets by post_id, return json format

--- app/controllers/API.php
+++ app/controllers/API.php
@@ -1,8 +1,10 @@
 <?php
+
 //class for returning json formatted data
 class API extends Controller
 {
     private $commentModel;
+
     public function __construct()
     {
         $this->commentModel = $this->model('Comment');
@@ -14,13 +16,20 @@ class API extends Controller
     }
 
 
-
-
-    public function comments()
+    public function comments($post_id = null)
     {
+        if ($post_id === null) {
+            header('Content-Type: application/json');
+            echo json_encode(['error' => 'no id given']);
+            die();
+        }
+        $comments = $this->commentModel->getMeComments($post_id);
         $data = [
-            'id' =>'comments'
+            'comments' => $comments,
+            'post_id' => $post_id
         ];
+        // KAD NARSYKLE ZINOTU, KAD CIA JSON
+        header('Content-Type: application/json');
         echo json_encode($data);
     }
 }
