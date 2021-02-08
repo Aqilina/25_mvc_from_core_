688d7612ea8e47f0fc32c3d76e5de85e5b819608
akvilina <akvilina.lapenaite@gmail.com>
2021-02-08 09:27:17 +0200

    addCommentAsynch json

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -10,6 +10,7 @@ class Comment
         $this->db = new Database;
     }
 
+//    KIEKV KARTA, SIUNCIANT KOMENTARA, VYKDOMA SITA F-JA
     public function getMeComments($post_id)
     {
         $this->db->query('SELECT * FROM comments WHERE post_id = :id');
@@ -23,6 +24,23 @@ class Comment
             return $comments;
         }
         return false;
+    }
+
+//    GET DATA USING DATA
+    //@return boolean
+    public function addCommentToDb($data)
+    {
+        $this->db->query("INSERT INTO  comments (post_id, author, comment_body) VALUES (:post_id, :author, :comment_body)");
+//        bind values
+            $this->db->bind(':post_id', $data['postId']);
+            $this->db->bind(':author', $data['author']);
+            $this->db->bind(':comment_body', $data['commentBody']);
+
+            if ($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
     }
 
 }
