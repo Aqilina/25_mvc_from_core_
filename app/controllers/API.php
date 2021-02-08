d941c707bb93d4c9174efd953f743d32c0b6c6ec
akvilina <akvilina.lapenaite@gmail.com>
2021-02-08 09:27:51 +0200

    api addComment method errors and success

--- app/controllers/API.php
+++ app/controllers/API.php
@@ -36,18 +36,71 @@ class API extends Controller
     //GAUNAMA IS FETCH
     public function addComment($post_id = null)
     {
-        $result = [];
-        print_r($_POST);
-        echo 'addComment ' . $post_id;
+
+        $result = [
+            'errors' => []
+        ];
+
+//        REIKALINGA JS
+        if ($post_id === null) {
+            //reikalinga js
+            $result['errors'] = 'no id given';
+            redirect('/posts');
+            die();
+        }
+//        print_r($_POST);
+//        echo 'addComment ' . $post_id;
 
         //VALIDATE POST VALUES
-        $commentData = $_POST;//pravalytam $_POST
+        $vld = new Validation();
+
+        //IF REQUEST IS POST AND SANITIZED
+        if ($vld->ifRequestIsPostAndSanitize()) {
+
+            $data = [
+                'username' => trim($_POST['username']),
+                'commentBody' => trim($_POST['commentBody']),
+                'errors' => [
+                    'usernameErr' => '',
+                    'commentBodyErr' => '',
+                ],
+            ];
+
+//            AR TUSTI IMPUTAI
+            $data['errors']['usernameErr'] = $vld->validateEmpty($data['username'], 'Username cant\'t be empty');
+            $data['errors']['commentBodyErr'] = $vld->validateEmpty($data['commentBody'], 'Please enter your comment');
+
+            //IF NO ERRORS
+            if ($vld->ifEmptyArr($data['errors'])) {
+                $commentData = [
+                    'author' => $data['username'],
+                    'commentBody' => $data['commentBody'],
+                    'postId' => $post_id
+                ];
+                //CREATE RESULT
+                if ( $this->commentModel->addCommentToDb($commentData)) {
+                    $result['success'] = 'Post added';
+
+                } else {
+                    $result['errors'] = 'Error adding post';
+                }
+
+            } else {
+            $result['errors'] = $data['errors'];
+            }
+//--------------------------------------------------------------------------------------------------
+//            IF REQUEST IS NOT POST
+        } else {
+            $result['error'] = 'not allowed';
+            redirect('/posts');
+        }
 
 
-        //IF VALID - ADD POST
+//        $commentData = $_POST;//pravalytam $_POST
 //        if ($this->commentModel->addCommentToDb($commentData)) return 'success';
+//--------------------------------------------------------------------------------------------------
 
-        $result['success'] = "Post added";
+        header('Content-Type: application/json');
         echo json_encode($result);
         die();
     }
